4689b1b1a965b6bd428fbfa1ea7ba1e9942afe33
ClarkFarnswoth <farnsworthclark@gmail.com>
2023-01-27 16:54:07 -0700

    finished the UI

--- CS4610-QuoteSearch/src/App.tsx
+++ CS4610-QuoteSearch/src/App.tsx
@@ -1,32 +1,49 @@
-import { useState } from 'react'
+import { useState, useEffect, KeyboardEvent } from 'react';
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+interface Quote{
+  description: string;
+  author: string;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [quotes, setQuote] = useState<Quote[]>([]);
+
+  const getQuote = async () => {
+    const result = await fetch("https://usu-quotes-mimic.vercel.app/api/random");
+    console.log(await result.json());
+    return(await result.json());
+  }
+  
+  useEffect( () => {
+    getQuote();
+
+  }, []);
+
+    
+  
+  
 
   return (
-    <div className="App">
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <h2>
+          Quote Search
+        </h2>
+        <div>
+        <input 
+        type="text"         
+        placeholder="Ken Block"
+        />
+        <button onClick={setQuote}>
+          Search
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        </div>
+        <div>
+          { getQuote().content }
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
